d50a85e7f7e08e00d7b9100255b1c1c37895c3ce
Farida-Ousmailou <faridaousmailou51@gmail.com>
2024-05-02 12:49:46 +0200

    add sort function

--- algo_filter/main.py
+++ algo_filter/main.py
@@ -1,7 +1,21 @@
 """This program allows you to bubble sort a table in ascending order"""
 
 
-def permutation(array, index) :
-    A = array[index]
-    array[index] = array[index+1]
-    array[index+1] = A
+def permutation(liste, index) :
+    A = liste[index]
+    liste[index] = liste[index+1]
+    liste[index+1] = A
+
+
+def sort(liste):
+    print("le table non ordoné : ", liste)
+    n = len(liste)
+    for i in range(n):
+      for j in range(0, n-i-1):
+        if liste[j] > liste[j+1]:
+            permutation(liste, j)
+        
+    return print("le table ordoné : ",liste)
+
+
+sort([23,40,2,6,98,97,500,0,5])
