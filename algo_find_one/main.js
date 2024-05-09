aace47d0b359f4aa603cbf5f3bdd314c9ece50d7
Farida-Ousmailou <faridaousmailou51@gmail.com>
2024-05-09 23:07:35 +0200

    add function get_vision

--- algo_find_one/main.js
+++ algo_find_one/main.js
@@ -1,16 +1,17 @@
 
 
-function find_one (number) {
+function find_digit_one (number) {
     if (number > 0){
-        const number = number.split("")
+        number_str = number.toString()
         console.log(number)
-        for(let i = 0; i < number.length; i++){
-            if(number[i]==1){
-                console.log(number[i])
-                return number[i]
+
+        for(let i = 0; i < number_str.length; i++){
+            if(parseInt(number_str[i])==1){
+                console.log((parseInt(number_str[i])))
+                return parseInt(number_str[i])
+
             } else {
-                console.log("Ce nombre ne contient pas de chiffre de 1")
-                
+                console.log("Ce nombre ne contient pas de chiffre de 1")    
             }
         }
     } else {
@@ -19,3 +20,25 @@ function find_one (number) {
     
 }
 
+// find_digit_one(231456)
+
+
+function get_vision (index, current_digit, number){
+    number_str = number.toString()
+    const start_index = (index - current_digit) > 0 ? index - current_digit : 0
+    const end_index = (index + current_digit) > 0 ? index + current_digit : 0
+
+    let vision = 0
+    for(let i = start_index; i <= end_index; i++){
+        if(i != index ) {
+            vision += parseInt(number_str[i])
+        }
+        
+    }
+    console.log(start_index)
+    console.log(end_index)
+    console.log(vision)
+    return vision
+}
+
+// get_vision(1, 3, 231456)
