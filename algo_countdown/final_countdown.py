7e260606e5d8f03d6bb45f3847fec514c8f135ba
Farida-Ousmailou <faridaousmailou51@gmail.com>
2024-05-30 13:02:59 +0200

    add docstring for this algo

--- algo_countdown/final_countdown.py
+++ algo_countdown/final_countdown.py
@@ -1,5 +1,24 @@
 import sys
 import math
+
+"""
+    Effectue un compte à rebours depuis un nombre donné jusqu'à zéro, puis affiche un message.
+
+    Args:
+        n (int): Le nombre de départ pour le compte à rebours. Doit être supérieur à 0.
+        message (str): Le message à afficher à la fin du compte à rebours.
+
+    Returns:
+        str: Retourne "Pas de compteur" si n est inférieur ou égal à 0.
+             Retourne "Pas de message" si le message est vide.
+
+    Comportement:
+        - Si n est supérieur ou égal à 60, affiche le temps restant en minutes et secondes.
+        - Si n est compris entre 1 et 10, affiche le temps restant suivi de "...".
+        - Sinon, affiche simplement le nombre restant.
+        - À la fin du compte à rebours, affiche le message en majuscules suivi d'un point d'exclamation.
+"""
+
                 
 
 def final_countdown(n, message) : 
